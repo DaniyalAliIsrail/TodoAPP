74aab69baabb1da778b20a922053888b7bb7ce9b
Daniyal ali <daniyalali12568@gmail.com>
2023-08-23 02:19:06 +0500

    todoApp

--- app.js
+++ app.js
@@ -1,40 +1,47 @@
-var list=document.getElementById("list");
 
+var list = document.getElementById("list");
 // adding a list using js
-function addList(){
-        var todoItems = document.getElementById("todo-items");
-        var todoItemsValue=todoItems.value;
-        var li = document.createElement("li");
-        var liText =document.createTextNode(todoItems.value);
-        li.appendChild(liText)
-        list.appendChild(li);
-
-        // del-button
-        var delBtn = document.createElement("button");
-        var delBtnText=document.createTextNode("DEL");
-        delBtn.appendChild(delBtnText)
-        li.appendChild(delBtn)
-        delBtn.setAttribute("onclick","deleteItems(this)");
-
-         //  edit-button
-        var  editBtn = document.createElement("button");
-        var editBtnText=document.createTextNode("Edit");
-        editBtn.appendChild(editBtnText);
-        li.appendChild(editBtn);
-        editBtn.setAttribute("onclick","editItems(this)")
-        todoItems.value=""
+const btn=document.getElementById("addList1")
+btn.addEventListener("click",addList)
+function addList() {
+  var todoItems = document.getElementById("todo-items");
+  var todoItemsvalue = todoItems.value;
+//   console.log(todoItems);
+  if (todoItemsvalue === "") {
+    return Swal.fire({
+      icon: 'error',
+      title: 'fill up input filled',
+      text: 'Add todo',
+      footer: '<a href="">Why do I have this issue?</a>'
+    })
+  }
+ var  todo =` <li id="dynamiclist">
+  <div>
+  ${todoItems.value}
+  </div>
+ <div class="btnlist">
+  <button  onclick="deleteItems(this)">DEL</button>
+  <button onclick="editItems(this)">Edit</button>
+ </div>
+</li>`
+list.innerHTML = todo + list.innerHTML;
+todoItems.value="";
+}
+function deleteItems(e) {
+  var delValue = e.parentNode.parentNode.remove();
 }
 
-function deleteItems(e){
-        var delValue = e.parentNode.remove();
+function editItems(e) {
+  var val = e.parentNode.parentNode.firstElementChild.textContent;
+  console.log(val);
+  var editValue = prompt("update value",val)
+  e.parentNode.parentNode.firstElementChild.textContent=editValue;
 }
-function editItems(e){
-    var val=e.parentNode.firstChild.nodeValue;
-    var editValue = prompt("Edit todo application",val)
-     e.parentNode.firstChild.nodeValue = editValue;
+const btn1=document.getElementById("deleteAll1")
+btn1.addEventListener("click",deleteAll)
+function deleteAll() {
+  var list = document.getElementById("list");
+  list.innerHTML = ""; // Set the innerHTML property to an empty string
 }
-
- function deleteAll() {
-        var list = document.getElementById("list")
-        list.innerHTML = ""; // Set the innerHTML property to an empty string
-     }
+window.deleteItems=deleteItems;
+window.editItems=editItems;
